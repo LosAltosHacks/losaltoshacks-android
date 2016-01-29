3ec3a5e7498419e06ab41af376f083375e338987
Ryan Huang <NPN@users.noreply.github.com>
2016-01-29 02:29:50 -0800

    Remove main menu

--- app/src/main/java/com/losaltoshacks/android/MainActivity.java
+++ app/src/main/java/com/losaltoshacks/android/MainActivity.java
@@ -20,8 +20,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -79,30 +77,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d(LOG_TAG, "Opening updates fragment because notification was opened.");
             mViewPager.setCurrentItem(2);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Log.d(LOG_TAG, "Pushed settings");
-            SyncAdapter.syncImmediately(this);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void checkIfPlayServicesAvailable() {
