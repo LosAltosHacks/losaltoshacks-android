c952e12e2d46d886e0cefd03410532fc59d353e7
Ryan Huang <NPN@users.noreply.github.com>
2017-01-31 23:55:47 -0800

    Remove segmented control, replace maps

--- app/src/main/java/com/losaltoshacks/android/MapFragment.java
+++ app/src/main/java/com/losaltoshacks/android/MapFragment.java
@@ -12,17 +12,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioGroup;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
-import info.hoang8f.android.segmented.SegmentedGroup;
-
-public class MapFragment extends Fragment implements RadioGroup.OnCheckedChangeListener {
+public class MapFragment extends Fragment {
     private static final String LOG_TAG = MapFragment.class.getSimpleName();
-    private SubsamplingScaleImageView mMapFloor1;
-    private SubsamplingScaleImageView mMapFloor2;
+    private SubsamplingScaleImageView mMap;
 
     public MapFragment() {
     }
@@ -34,28 +30,9 @@ public class MapFragment extends Fragment implements RadioGroup.OnCheckedChangeL
 
         final View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        SegmentedGroup segmentedGroup = (SegmentedGroup) view.findViewById(R.id.segmentMapControl);
-        segmentedGroup.setOnCheckedChangeListener(this);
-
-        mMapFloor1 = (SubsamplingScaleImageView) view.findViewById(R.id.map_floor1);
-        mMapFloor1.setImage(ImageSource.resource(R.drawable.floor1));
-
-        mMapFloor2 = (SubsamplingScaleImageView) view.findViewById(R.id.map_floor2);
-        mMapFloor2.setImage(ImageSource.resource(R.drawable.floor2));
+        mMap = (SubsamplingScaleImageView) view.findViewById(R.id.map);
+        mMap.setImage(ImageSource.resource(R.drawable.map));
 
         return view;
-    }
-
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        switch(checkedId) {
-            case R.id.buttonFloor1:
-                mMapFloor1.setVisibility(View.VISIBLE);
-                mMapFloor2.setVisibility(View.GONE);
-                break;
-            case R.id.buttonFloor2:
-                mMapFloor1.setVisibility(View.GONE);
-                mMapFloor2.setVisibility(View.VISIBLE);
-                break;
-        }
     }
 }
